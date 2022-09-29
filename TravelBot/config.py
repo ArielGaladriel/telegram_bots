b92df9659bd38b11beb6f88651d51fe346b14620
ArielGaladriel <arielbagdasarov@gmail.com>
2022-09-30 01:19:05 +0400

    TravelBot v.0.0.6: update files for deployment heroku

--- TravelBot/config.py
+++ TravelBot/config.py
@@ -5,3 +5,11 @@ load_dotenv()
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 WEATHER_API = str(os.getenv("WEATHER_API"))
 CURRENCY_API = str(os.getenv("CURRENCY_API"))
+
+HEROKU_APP_NAME = str(os.getenv('HEROKU_APP_NAME'))
+TOKEN = str(os.getenv("TOKEN"))
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
--- TravelBot/loader.py
+++ TravelBot/loader.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import start_webhook
 
 import config
 
@@ -9,6 +10,24 @@ bot = Bot(token=config.BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(config.WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
 if __name__ == '__main__':
     from handlers import dp
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=config.WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=config.WEBAPP_HOST,
+        port=config.WEBAPP_PORT,
+    )
+    # executor.start_polling(dp, skip_updates=True)
